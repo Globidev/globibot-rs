a441b6ab37442c5afb25e4f79de3f05ddf0d92f0
Guillaume Depardon <guillaume.depardon@gmail.com>
2022-04-17 10:43:01 +0200

    feat(plugin-common): Reexport anyhow and provide env utility

--- globibot-plugin-common/src/lib.rs
+++ globibot-plugin-common/src/lib.rs
@@ -1,1 +1,9 @@
 pub mod imageops;
+
+pub use anyhow;
+pub use image;
+
+pub fn load_env(key: &str) -> String {
+    std::env::var(key)
+        .unwrap_or_else(|why| panic!("Failed to load environment variable '{key}': {why}"))
+}
