bb49272b04a3efb56f055e6645e80133efc492d7
Globidev <globi@locomotive.eu>
2026-01-17 23:29:00 +0100

    add a dedicated page for the LLM plugin

--- web/src/routes/plugins/[slug]/+page.svelte
+++ web/src/routes/plugins/[slug]/+page.svelte
@@ -12,18 +12,22 @@
     Plugin '{params.slug}' not found.
   </div>
 {:else}
-  <div class="mx-auto max-w-5xl px-6 py-16">
+  <div class="mx-auto max-w-5xl px-6">
     <!-- Header -->
     <header class="mb-12 text-center">
-      <h1 class="text-2xl font-semibold tracking-tight">{$plugin.name}</h1>
+      <h1 class="text-2xl font-semibold tracking-tight">{$plugin.name} plugin</h1>
     </header>
 
-    <iframe
+    <div class="flex flex-col items-center gap-6 text-center">
+      <span>This plugin does not expose a Web UI</span>
+    </div>
+
+    <!-- <iframe
       class="border border-amber-50"
       width="100%"
       height="600"
       title="Plugin UI"
       src={`/plugin-api/${$plugin.name}/`}
-    ></iframe>
+    ></iframe> -->
   </div>
 {/if}
---
+++ web/src/routes/plugins/llm/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import DiscordLogin from '../../../lib/components/DiscordLogin.svelte';
+  import { authStore } from '../../../lib/stores/auth.svelte';
+</script>
+
+<div class="mx-auto max-w-5xl px-6">
+  <!-- Header -->
+  <header class="mb-12 text-center">
+    <h1 class="text-2xl font-semibold tracking-tight">LLM plugin</h1>
+  </header>
+
+  <div class="flex flex-col items-center gap-6 text-center">
+    {#if authStore.user == null}
+      <div class="text-sm">You must be logged in to access the LLM plugin.</div>
+      <DiscordLogin />
+    {:else}
+      <div class="text-9xl">🚧</div>
+    {/if}
+  </div>
+</div>
