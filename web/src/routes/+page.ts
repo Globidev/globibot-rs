93a303827ea66c133ea88fe4380e36c9ab44fe1c
Globidev <globi@locomotive.eu>
2026-01-17 23:28:31 +0100

    remove dummy timeout

--- web/src/routes/+page.ts
+++ web/src/routes/+page.ts
@@ -1,5 +1,5 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad<void> = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
 };
